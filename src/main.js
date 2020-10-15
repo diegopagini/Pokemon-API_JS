cb239194472bd0fbdbed692f81dc8fb95cbcfe4f
unknown <diegopagini777@gmail.com>
2020-10-15 15:50:36 -0300

    cambio n° 11 de 999

--- src/main.js
+++ src/main.js
@@ -6,3 +6,31 @@ function callAPI() {
     .then((data) => console.log(data.results));
   }
   callAPI();
+  //Anda pero no la puedo manipular.
+
+
+
+function callAPI2() {
+  fetch("https://pokeapi.co/api/v2/pokemon")
+    .then((response) => response.json())
+    .then((data) => {
+      return data.results = pokemonsList
+    });
+    console.log(pokemonsList)
+}
+callAPI2();
+//No anda, algo estoy haciendo mal
+
+
+
+let pokemonArray = [];
+function callAPI3() {
+  fetch("https://pokeapi.co/api/v2/pokemon")
+    .then((response) => response.json())
+    .then((data) => {
+      pokemonArray = data.results;
+    });
+    console.log(pokemonArray);
+}
+callAPI3();
+//Pues tampoco...
