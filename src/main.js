ce9abeb2d69f4b8fd27d8ed85d07cafd2593e823
Diego Pagini <diegopagini777@gmail.com>
2020-10-21 16:17:32 -0300

    Slide modificado, y función para traer nombres

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
 // Api
 function callAPI() {
+  let pokemons = '';
+
   dataEndpoint = fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=200`)
     .then((response) => response.json())
     .then((pokeData) => {
       let pokeResults = pokeData.results;
-      pokeResults.forEach(element => {
-        console.log(element.name)
-        console.log(element.url)
+      pokeResults.forEach((element) => {
+        //Si quiero independizar la parte de mostrar del resto de la function, como hago?
+
+        pokemons += `
+        <div class="col m2 center">
+        <h5>${element.name}</h5>
+        <span><img src="${element.url}" alt="pokemon"></span>
+        </div>
+        `;
+
+        // console.log(element.name)
+        // console.log(element.url)
+
+        document.getElementById("pokedex").innerHTML = pokemons;
       });
     });
-  }
+}
 
-callAPI()
+callAPI();
