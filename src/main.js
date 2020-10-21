1ffefe8a10724d384a4877ca0ac25118b2d44b3e
Diego Pagini <diegopagini777@gmail.com>
2020-10-21 15:44:18 -0300

    Ya atrape a los pokemon

--- src/main.js
+++ src/main.js
@@ -1,36 +1,14 @@
 // Api
-let pokemonsList = [];
 function callAPI() {
-  fetch("https://pokeapi.co/api/v2/pokemon")
+  dataEndpoint = fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=200`)
     .then((response) => response.json())
-    .then((data) => console.log(data.results));
+    .then((pokeData) => {
+      let pokeResults = pokeData.results;
+      pokeResults.forEach(element => {
+        console.log(element.name)
+        console.log(element.url)
+      });
+    });
   }
-  callAPI();
-  //Anda pero no la puedo manipular.
 
-
-
-function callAPI2() {
-  fetch("https://pokeapi.co/api/v2/pokemon")
-    .then((response) => response.json())
-    .then((data) => {
-      return data.results = pokemonsList
-    });
-    console.log(pokemonsList)
-}
-callAPI2();
-//No anda, algo estoy haciendo mal
-
-
-
-let pokemonArray = [];
-function callAPI3() {
-  fetch("https://pokeapi.co/api/v2/pokemon")
-    .then((response) => response.json())
-    .then((data) => {
-      pokemonArray = data.results;
-    });
-    console.log(pokemonArray);
-}
-callAPI3();
-//Pues tampoco...
+callAPI()
